c4bac47aac0a6a0a37c40a57ddc19b4f87df66a4
dasalebr <65932591+dasalebr@users.noreply.github.com>
2021-02-10 12:36:12 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,115 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {}
+variable "region" {
+  default = "us-east-1"
+}
+variable "subnet" {
+  default = "10.0.0.0/24"
+  }
+  variable "cidr_block" {
+    default = "10.0.0.0/16"
+  }
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  access_key = var.aws_access_key   #syntax 
+  secret_key = var.aws_secret_key
+  region     = var.region
+}
+
+##################################################################################
+# DATA
+##################################################################################
+#pulling data from AWS - latest
+data "aws_ami" "aws-linux" {     
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+##################################################################################
+# RESOURCES
+##################################################################################
+#this uses the default VPC. It WILL NOT delete it on destroy.
+ 
+resource "aws_default_vpc" "default" {  
+   
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "nginx_demo"
+  description = "Allow ports for nginx demo"
+  vpc_id      = aws_default_vpc.default.id    #triplet
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami                    = data.aws_ami.aws-linux.id   #pulling ami id from data source"
+  instance_type          = "t2.micro"
+  key_name               = var.key_name    #variable key name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]  #what vpc id - defines list
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.private_key_path) #containes private key path
+
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start"
+    ]
+  }
+}
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "aws_instance_public_dns" {
+  value = aws_instance.nginx.public_dns
+}
---
+++ terraform.tfvars
@@ -0,0 +1,13 @@
+# IAM Access and Secret Key for your IAM user
+aws_access_key = ""
+
+aws_secret_key = ""
+
+# Name of the key pair in AWS, MUST be in same region as EC2 instance
+# Check README for AWS CLI commands to create a key pair
+key_name = "MyKey"
+
+# Local path to pem file for key pair. 
+# Windows paths need to use double-backslash: Ex. C:\\Users\\dlemos\\test.pem
+private_key_path = "C:\\Users\\dlemos\\Pictures\\Getting-Started-terraform\\m3\\MyKey.pem"
+
